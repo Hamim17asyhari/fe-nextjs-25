92d363ad24c44057357076dfc1406c80d0dd94c2
Hamim17asyhari <163313608+Hamim17asyhari@users.noreply.github.com>
2025-12-12 15:39:35 +0700

    ksksj

---
+++ baru/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Layout from "./componen/ui/Layout";
+import Button from "./componen/ui/Button";
+
+export default function Home() {
+  return (
+   <Layout>
+    <h1>Hello </h1>
+    <Button />
+   </Layout>
+   
+  );
+}
