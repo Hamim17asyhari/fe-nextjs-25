69c1b4b59dc570942d5fe4ddd01971c68aa82478
Hamim17asyhari <163313608+Hamim17asyhari@users.noreply.github.com>
2026-01-06 10:15:53 +0700

    Refactor product category creation form layout
    - Adjusted the structure of the product category creation page by moving the <form> tag to ensure proper layout.
    - Maintained the existing input fields for name and description.

--- app/produkcategory/create/page.tsx
+++ app/produkcategory/create/page.tsx
@@ -4,8 +4,8 @@ import { Button, TextField } from "@mui/material";
 
 export default function Page() {
     return <Layout>
-        <form>
         <h1 className='text-2xl font-bold text-black'> Create Product Category </h1>
+        <form>
             <div className="grid grid-cols-4 gap-4 my-4">
                 <TextField name="name" id="name" label="Name" variant="standard" />
                 <TextField 
